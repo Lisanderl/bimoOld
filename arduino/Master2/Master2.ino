59ec682eb7b5cb92aa18bc32ff7b57170f93caa2
vobidniak <vobidniak@homeaway.com>
2018-03-16 18:30:14 +0200

    change logic in slave

--- arduino/Master2/Master2.ino
+++ arduino/Master2/Master2.ino
@@ -5,12 +5,10 @@
 #include <RF24_config.h>
 #include <SPI.h>
 
-
 RF24 radio(A0, A1);
 float const ADC1V1 = 0.02275;
 float const ADC5V0 = 0.11556;
 float battaryVoltage = 0.0;
-char serialData[32];
 int ledPin = 2;
 int counter = 0; 
 
@@ -19,19 +17,15 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(76800);
   nrfSetup();
-  radio.write(serialData, 1);
+//  radio.write(serialData, 1);
   radio.startListening();
   
 }
 void loop() {
-Serial.print(sizeof(data1));
-delay(2);
-Serial.print(data1);
-delay(2000);
-
+  while(true){
   if (radio.available()) {
     char len = radio.getDynamicPayloadSize();
-    radio.read(data1, len);
+//    radio.read(data1, len);
    // Serial.println(data1);
  //  if(counter==1){
 //    battaryVoltage = (float) atoi(data1)*ADC1V1;
@@ -43,32 +37,33 @@ delay(2000);
 }
 
 void serialEvent() {
-  bool status;
+  char serialData[32];
+  int i = 0;
   radio.stopListening();
-  serialData = Serial.read();
-  status = radio.write(serialData, 1);
-  if(serialData[0]=='G'){radio.startListening();
-  Serial.print(battaryVoltage);
-  delay(10);
-  Serial.print(solarVoltage);
-  }
- // Serial.println(status);
-
-  
-  delay(1);
-
-
+ while(Serial.available()){
+  serialData[i] = Serial.read();
+  i++;
+ }
+ bool requestStatus = radio.write(serialData, sizeof(serialData));
+ delay(1); 
+Serial.print(jsonParser("WriteToSlave", (String)requestStatus));
 }
 
+void blinc(int ms) {
 
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(ms);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(ms);
+  char serialData[0];
+  sizeof(serialData);
+}
 
 void nrfSetup() {
   bool master = true;
-  
   byte addr1[] = {0xC2, 0xC2, 0xC2, 0xC2, 0xC2};
   byte addr2[] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
-
-  uint8_t cnt;
+  
   radio.begin();
   radio.txDelay = 135;
   radio.enableDynamicPayloads();
@@ -87,15 +82,10 @@ void nrfSetup() {
   radio.stopListening();
 }
 
-void blinc(int ms) {
-
-  digitalWrite(ledPin, HIGH);   // sets the LED on
-  delay(ms);                  // waits for a second
-  digitalWrite(ledPin, LOW);    // sets the LED off
-  delay(ms);
-  char serialData[0];
-  sizeof(serialData);
+String jsonParser(String name, String value){
+  return "{\""+name+"\":"+value+"}";
 }
+
 
 
 
--- arduino/Slave2/Slave2.ino
+++ arduino/Slave2/Slave2.ino
@@ -1,4 +1,4 @@
-
+#include <ArduinoJson.h>
 #include <BimoControl.h>
 #include <nRF24L01.h>
 #include <printf.h>
@@ -8,10 +8,11 @@
 #include <Ultrasonic.h>
 
 #define CONTROLS (d=='W'||d=='S'||d=='A'||d=='D'||d=='C'||d=='L')
+
 //motors control
 Motor m1(10, 4, 3);
 Motor m2(9, 7, 8);
-BimoControl bimo(m1,m2);
+BimoControl bimo(m1, m2);
 //NRF24
 RF24 radio(A0, A1);
 char data[32];
@@ -20,21 +21,22 @@ int d ;
 int trigPin = A3;
 int echoPin = A2;
 Ultrasonic ultrasonic(trigPin, echoPin);
-int distance=5;
+int distance = 5;
 //led/tone control
 int ledPin = 2;// for tests
 int led1Pin = 5;
 int led2Pin = 6;
 int tonePin = A5;
 unsigned int led1Mode = 0;
-
+DynamicJsonBuffer jsonBuffer(128);
 
 void setup() {
-configs();
+  configs();
 }
 
 void loop() {
-distanceReaction();
+  while(true){
+  distanceReaction();
 
   if (radio.available()) {
     bimo.blinc(5);
@@ -46,7 +48,7 @@ distanceReaction();
       interpretator();
     } else if (d == 'G') {
       char str[4];
-      int i=analogRead(7);
+      int i = analogRead(7);
       bimo.blinc(5);
       //sendADC();
       itoa(i, str, 10);
@@ -54,18 +56,27 @@ distanceReaction();
     }
   }
 
-  if(distance<6){
-  bimo.setMotorPWM(70,70); bimo.goBack();
-  delay(random(10, 150));
-  bimo.setMotorPWM(100,100); bimo.goBack();
-  delay(random(10, 150));
-  bimo.setMotorPWM(130,130); bimo.goBack();
-  delay(random(10, 100));
-  tone(tonePin, 700, 50);
-  bimo.stopMove();
- } 
+  if (distance < 6) {
+    bimo.setMotorPWM(70, 70); bimo.goBack();
+    delay(random(10, 150));
+    bimo.setMotorPWM(100, 100); bimo.goBack();
+    delay(random(10, 150));
+    bimo.setMotorPWM(130, 130); bimo.goBack();
+    delay(random(10, 100));
+    tone(tonePin, 700, 50);
+    bimo.stopMove();
+  }
 }
-
+}
+void parseDataFromRadio() {
+  char requestData[32];
+  if (radio.available()) {
+    int len = radio.getDynamicPayloadSize();
+    radio.read(requestData, len);
+    JsonObject& root = jsonBuffer.parseObject(requestData);
+  }
+}
+//TO DO: creat default values of adjustments like pwm and adc...
 void sendADC() {
   int sizeADC;
   int i;
@@ -95,41 +106,54 @@ void sendADC() {
 void interpretator() {
 
   switch (d) {
-    case '1':  if(distance>5){ bimo.setMotorPWM(150,150); bimo.goStraight(); }
+    case '1':  if (distance > 5) {
+        bimo.setMotorPWM(150, 150);
+        bimo.goStraight();
+      }
       break;
-    case '2':  bimo.setMotorPWM(130,130); bimo.goBack();
+    case '2':  bimo.setMotorPWM(130, 130); bimo.goBack();
       break;
-    case '3':  bimo.setMotorPWM(130,130); bimo.goLeft();
+    case '4':  bimo.setMotorPWM(130, 130); bimo.goLeft();
       break;
-    case '4':  bimo.setMotorPWM(130,130); bimo.goRight();
+    case '7':  bimo.setMotorPWM(130, 130); bimo.goRight();
       break;
-    case 'L': if (led1Mode == 5) {
+    case '5':  //set slowly right
+      break;
+    case '8':  //set slowly left
+      break;
+    case '6':  //set back slowly right
+      break;
+    case '9':  //set back slowly left
+      break;
+    case '20':   tone(tonePin, random(10, 800), 100);
+      break;
+    case '202': if (led1Mode == 5) {
         led1Mode = -1;
       } bimo.ledON(led1Pin, ++led1Mode);
       break;
-    default : bimo.setMotorPWM(0,0); bimo.stopMove();
+    default : bimo.setMotorPWM(0, 0); bimo.stopMove();
   }
 }
 
-void dataSendWrapper(char prefix, char data[], int masSize){
-char message[masSize+1];
-radio.stopListening();
-message[0]=prefix;
-for(int i=0; i<masSize; i++){
-  message[i+1]=data[i];
-}
+void dataSendWrapper(char prefix, char data[], int masSize) {
+  char message[masSize + 1];
+  radio.stopListening();
+  message[0] = prefix;
+  for (int i = 0; i < masSize; i++) {
+    message[i + 1] = data[i];
+  }
   radio.write(message, masSize);
   delay(1);
   radio.startListening();
 }
 
-void distanceReaction(){
- distance = (int) round(ultrasonic.Ranging(CM));
+void distanceReaction() {
+  distance = (int) round(ultrasonic.Ranging(CM));
 
 }
 
 void configs() {
-  
+
   bimo.setBlincPin(ledPin);
   TCCR1B = TCCR1B & 0b11111000 | 5; //set PWM to low frecency
   analogReference(INTERNAL);
@@ -157,10 +181,10 @@ void configs() {
 
 void nrfSetup() {
   bool master = false;
-  
+
   byte addr1[] = {0xC2, 0xC2, 0xC2, 0xC2, 0xC2};
   byte addr2[] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
-  
+
   radio.begin();
   radio.txDelay = 135;
   radio.enableDynamicPayloads();
@@ -171,11 +195,11 @@ void nrfSetup() {
   radio.setRetries(1, 3); // 1 - delay, 2 - amount
   radio.setAutoAck( true ) ;
 
-    radio.openWritingPipe(addr1);
-    radio.openReadingPipe(1, addr2);
- 
+  radio.openWritingPipe(addr1);
+  radio.openReadingPipe(1, addr2);
+
   radio.powerUp();
   bimo.blinc(100);
   radio.startListening();
-   bimo.blinc(10);
+  bimo.blinc(10);
 }
